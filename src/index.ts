83550d2cc3a56090e1ab340615be60ea8bc55d52
ushijimashougo <49727131+ushijimashougo@users.noreply.github.com>
2026-09-04 13:44:37 +0900

    feat: expose public library API

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { calculateTournament } from "./calculateTournament.js";
+export { getGradeInfo } from "./grade.js";
+
+export type {
+  FesaCalculationMethod,
+  FesaGame,
+  FesaGrade,
+  FesaGradeInfo,
+  FesaHandicap,
+  FesaPlayer,
+  FesaPlayerResult,
+  FesaRatingState,
+  FesaResult,
+  FesaTournamentInput,
+  FesaTournamentResult,
+  PriorRatedGame,
+} from "./types.js";
