130d58a6eaf0d93f6ebc1e1bd8525bd0c98003a9
ushijimashougo <49727131+ushijimashougo@users.noreply.github.com>
2026-09-04 14:13:25 +0900

    feat: expose handicap calculation utilities

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 export { calculateTournament } from "./calculateTournament.js";
 export { getGradeInfo } from "./grade.js";
+export {
+  handicapAdjustedRating,
+  handicapGradeEffect,
+  handicapRatingEffect,
+} from "./handicap.js";
 
 export type {
   FesaCalculationMethod,
